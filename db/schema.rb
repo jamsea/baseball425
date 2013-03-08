8ed9601f04d36aa8ed858ad94737f53a50e1a79f
Jamie Hush <hush@ltsp-bravo.csmanage.uwindsor.ca>
2013-03-07 22:52:58 -0500

    Created oracle branch. Modified config files to work with an oracle database.

--- db/schema.rb
+++ db/schema.rb
@@ -15,80 +15,145 @@ ActiveRecord::Schema.define(:version => 20130308030926) do
 
   create_table "batting_seasons", :force => true do |t|
     t.string   "team"
-    t.integer  "jersey_number"
-    t.integer  "season_order"
+    t.integer  "jersey_number", :precision => 38, :scale => 0
+    t.integer  "season_order",  :precision => 38, :scale => 0
     t.string   "position"
-    t.integer  "ab"
-    t.integer  "gp"
-    t.integer  "pa"
-    t.integer  "singles"
-    t.integer  "doubles"
-    t.integer  "triples"
-    t.integer  "hr"
-    t.integer  "tb"
-    t.integer  "r"
-    t.integer  "rbi"
-    t.integer  "bb"
-    t.integer  "ibb"
-    t.integer  "so"
-    t.integer  "hbp"
-    t.integer  "sf"
-    t.integer  "sh"
-    t.integer  "gidp"
-    t.integer  "sb"
-    t.integer  "cs"
-    t.float    "ba"
-    t.float    "obp"
-    t.float    "slg"
-    t.float    "ops"
-    t.float    "k_percent"
-    t.float    "bb_percent"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.integer  "ab",            :precision => 38, :scale => 0
+    t.integer  "gp",            :precision => 38, :scale => 0
+    t.integer  "pa",            :precision => 38, :scale => 0
+    t.integer  "singles",       :precision => 38, :scale => 0
+    t.integer  "doubles",       :precision => 38, :scale => 0
+    t.integer  "triples",       :precision => 38, :scale => 0
+    t.integer  "hr",            :precision => 38, :scale => 0
+    t.integer  "tb",            :precision => 38, :scale => 0
+    t.integer  "r",             :precision => 38, :scale => 0
+    t.integer  "rbi",           :precision => 38, :scale => 0
+    t.integer  "bb",            :precision => 38, :scale => 0
+    t.integer  "ibb",           :precision => 38, :scale => 0
+    t.integer  "so",            :precision => 38, :scale => 0
+    t.integer  "hbp",           :precision => 38, :scale => 0
+    t.integer  "sf",            :precision => 38, :scale => 0
+    t.integer  "sh",            :precision => 38, :scale => 0
+    t.integer  "gidp",          :precision => 38, :scale => 0
+    t.integer  "sb",            :precision => 38, :scale => 0
+    t.integer  "cs",            :precision => 38, :scale => 0
+    t.decimal  "ba"
+    t.decimal  "obp"
+    t.decimal  "slg"
+    t.decimal  "ops"
+    t.decimal  "k_percent"
+    t.decimal  "bb_percent"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+  end
+
+  create_table "department", :primary_key => "department_id", :force => true do |t|
+    t.string "name",     :limit => 30, :null => false
+    t.string "location", :limit => 30
+  end
+
+  create_table "employee", :primary_key => "employee_id", :force => true do |t|
+    t.string   "last_name",     :limit => 10
+    t.string   "first_name",    :limit => 10
+    t.string   "sin_number",    :limit => 9
+    t.datetime "date_of_birth"
+    t.integer  "department_id", :limit => 2,  :precision => 2, :scale => 0
+    t.string   "email",         :limit => 30
+  end
+
+  create_table "inv", :primary_key => "partno", :force => true do |t|
+    t.string  "description", :limit => 25
+    t.integer "qonhand",     :limit => 5,  :precision => 5, :scale => 0
+  end
+
+  create_table "j", :primary_key => "jno", :force => true do |t|
+    t.string "jname", :limit => 10
+    t.string "city",  :limit => 10
+  end
+
+  create_table "p", :primary_key => "pno", :force => true do |t|
+    t.string  "pname",  :limit => 10
+    t.string  "color",  :limit => 5
+    t.decimal "weight",               :precision => 5, :scale => 2
+    t.string  "city",   :limit => 10
   end
 
   create_table "pitching_seasons", :force => true do |t|
     t.string   "team"
-    t.integer  "jersey_number"
-    t.integer  "season_order"
-    t.integer  "gp"
-    t.integer  "gs"
-    t.integer  "w"
-    t.integer  "l"
-    t.integer  "sv"
-    t.integer  "bs"
-    t.integer  "hld"
-    t.integer  "outs"
-    t.integer  "tbf"
-    t.integer  "h"
-    t.integer  "r"
-    t.integer  "er"
-    t.integer  "hr"
-    t.integer  "k"
-    t.integer  "bb"
-    t.integer  "ibb"
-    t.integer  "hbp"
-    t.integer  "cg"
-    t.integer  "sho"
-    t.integer  "wp"
-    t.integer  "bk"
-    t.float    "era"
-    t.float    "whip"
-    t.float    "k_percent"
-    t.float    "bb_percent"
-    t.float    "babip"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.integer  "jersey_number", :precision => 38, :scale => 0
+    t.integer  "season_order",  :precision => 38, :scale => 0
+    t.integer  "gp",            :precision => 38, :scale => 0
+    t.integer  "gs",            :precision => 38, :scale => 0
+    t.integer  "w",             :precision => 38, :scale => 0
+    t.integer  "l",             :precision => 38, :scale => 0
+    t.integer  "sv",            :precision => 38, :scale => 0
+    t.integer  "bs",            :precision => 38, :scale => 0
+    t.integer  "hld",           :precision => 38, :scale => 0
+    t.integer  "outs",          :precision => 38, :scale => 0
+    t.integer  "tbf",           :precision => 38, :scale => 0
+    t.integer  "h",             :precision => 38, :scale => 0
+    t.integer  "r",             :precision => 38, :scale => 0
+    t.integer  "er",            :precision => 38, :scale => 0
+    t.integer  "hr",            :precision => 38, :scale => 0
+    t.integer  "k",             :precision => 38, :scale => 0
+    t.integer  "bb",            :precision => 38, :scale => 0
+    t.integer  "ibb",           :precision => 38, :scale => 0
+    t.integer  "hbp",           :precision => 38, :scale => 0
+    t.integer  "cg",            :precision => 38, :scale => 0
+    t.integer  "sho",           :precision => 38, :scale => 0
+    t.integer  "wp",            :precision => 38, :scale => 0
+    t.integer  "bk",            :precision => 38, :scale => 0
+    t.decimal  "era"
+    t.decimal  "whip"
+    t.decimal  "k_percent"
+    t.decimal  "bb_percent"
+    t.decimal  "babip"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
   end
 
   create_table "players", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.date     "date_of_birth"
-    t.float    "height"
-    t.float    "weight"
+    t.datetime "date_of_birth"
+    t.decimal  "height"
+    t.decimal  "weight"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "quot", :id => false, :force => true do |t|
+    t.integer "suppno",        :limit => 5,  :precision => 5,  :scale => 0, :null => false
+    t.integer "partno",        :limit => 10, :precision => 10, :scale => 0, :null => false
+    t.decimal "price",                       :precision => 7,  :scale => 2
+    t.integer "delivery_time", :limit => 2,  :precision => 2,  :scale => 0
+    t.integer "qonorder",      :limit => 3,  :precision => 3,  :scale => 0
+  end
+
+  create_table "quotations", :id => false, :force => true do |t|
+    t.integer "sno",      :precision => 38, :scale => 0, :null => false
+    t.integer "pno",      :precision => 38, :scale => 0, :null => false
+    t.decimal "price"
+    t.integer "del_time", :precision => 38, :scale => 0
+    t.integer "on_order", :precision => 38, :scale => 0
+  end
+
+  create_table "s", :primary_key => "sno", :force => true do |t|
+    t.string  "sname",  :limit => 10
+    t.integer "status", :limit => 5,  :precision => 5, :scale => 0
+    t.string  "city",   :limit => 10
+  end
+
+  create_table "spj", :id => false, :force => true do |t|
+    t.string  "sno", :limit => 3,                               :null => false
+    t.string  "pno", :limit => 3,                               :null => false
+    t.string  "jno", :limit => 3,                               :null => false
+    t.integer "qty", :limit => 3, :precision => 3, :scale => 0
+  end
+
+  create_table "supp", :primary_key => "supno", :force => true do |t|
+    t.string "name",    :limit => 25
+    t.string "address", :limit => 35
   end
 
   create_table "teams", :force => true do |t|
@@ -100,5 +165,15 @@ ActiveRecord::Schema.define(:version => 20130308030926) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  add_foreign_key "quot", "inv", :column => "partno", :primary_key => nil, :name => "fk_partno_in_quot"
+  add_foreign_key "quot", "supp", :column => "suppno", :primary_key => nil, :name => "fk_suppno_in_quot"
+
+  add_foreign_key "quotations", "inv", :column => "pno", :primary_key => nil, :name => "part#_fk"
+  add_foreign_key "quotations", "supp", :column => "sno", :primary_key => nil, :name => "supp#_fk"
+
+  add_foreign_key "spj", "j", :column => "jno", :primary_key => nil, :name => "fk_jno_in_j"
+  add_foreign_key "spj", "p", :column => "pno", :primary_key => nil, :name => "fk_pno_in_p"
+  add_foreign_key "spj", "s", :column => "sno", :primary_key => nil, :name => "fk_sno_in_s"
 
 end
